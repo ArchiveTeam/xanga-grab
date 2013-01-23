6a7723ac0f863e41fe55b255022c24734e7fc239
Alard <alard@example.com>
2013-01-23 17:24:29 +0100

    A pipeline.

---
+++ pipeline.py
@@ -0,0 +1,137 @@
+import os
+import os.path
+import shutil
+import time
+from distutils.version import StrictVersion
+
+import seesaw
+from seesaw.project import *
+from seesaw.config import *
+from seesaw.item import *
+from seesaw.task import *
+from seesaw.pipeline import *
+from seesaw.externalprocess import *
+from seesaw.tracker import *
+from seesaw.util import find_executable
+
+
+if StrictVersion(seesaw.__version__) < StrictVersion("0.0.12"):
+  raise Exception("This pipeline needs seesaw version 0.0.12 or higher.")
+
+WGET_LUA = find_executable("Wget+Lua",
+    "GNU Wget 1.14.lua.20130120-8476",
+    [ "./wget-lua",
+      "./wget-lua-warrior",
+      "./wget-lua-local",
+      "../wget-lua",
+      "../../wget-lua",
+      "/home/warrior/wget-lua",
+      "/usr/bin/wget-lua" ])
+
+if not WGET_LUA:
+  raise Exception("No usable Wget+Lua found.")
+
+
+USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
+VERSION = "20130123.01"
+
+
+class PrepareDirectories(SimpleTask):
+  def __init__(self, warc_prefix):
+    SimpleTask.__init__(self, "PrepareDirectories")
+    self.warc_prefix = warc_prefix
+
+  def process(self, item):
+    item_name = item["item_name"]
+    dirname = "/".join(( item["data_dir"], item_name ))
+
+    if os.path.isdir(dirname):
+      shutil.rmtree(dirname)
+    os.makedirs(dirname)
+
+    item["item_dir"] = dirname
+    item["warc_file_base"] = "%s-%s-%s" % (self.warc_prefix, item_name, time.strftime("%Y%m%d-%H%M%S"))
+
+    open("%(item_dir)s/%(warc_file_base)s.warc.gz" % item, "w").close()
+
+class MoveFiles(SimpleTask):
+  def __init__(self):
+    SimpleTask.__init__(self, "MoveFiles")
+
+  def process(self, item):
+    os.rename("%(item_dir)s/%(warc_file_base)s.warc.gz" % item,
+              "%(data_dir)s/%(warc_file_base)s.warc.gz" % item)
+
+    shutil.rmtree("%(item_dir)s" % item)
+
+
+
+project = Project(
+  title = "Xanga",
+  project_html = """
+    <img class="project-logo" alt="Weblog.nl logo" src="http://archiveteam.org/images/4/4d/Xanga-logo-main.gif" width="120" />
+    <h2>Xanga.com <span class="links"><a href="http://www.xanga.com/">Website</a> &middot; <a href="http://tracker.archiveteam.org/xanga/">Leaderboard</a></span></h2>
+    <p><i>Xanga</i> is getting old. Archive Team investigates.</p>
+  """
+  # , utc_deadline = datetime.datetime(2013,03,01, 23,59,0)
+)
+
+TRACKER_ID = "xanga"
+RSYNC_TARGET = ConfigInterpolation("fos.textfiles.com::alardland/warrior/xanga/%s/", downloader)
+
+pipeline = Pipeline(
+  GetItemFromTracker("http://tracker.archiveteam.org/%s" % TRACKER_ID, downloader, VERSION),
+  PrepareDirectories(warc_prefix="xanga.com"),
+  WgetDownload([ WGET_LUA,
+      "-U", USER_AGENT,
+      "-nv",
+      "-o", ItemInterpolation("%(item_dir)s/wget.log"),
+      "--lua-script", "xanga.lua",
+      "--no-check-certificate",
+      "--output-document", ItemInterpolation("%(item_dir)s/wget.tmp"),
+      "--truncate-output",
+      "-e", "robots=off",
+      "--rotate-dns",
+      "--recursive", "--level=inf",
+      "--page-requisites",
+      "--timeout", "60",
+      "--tries", "20",
+      "--waitretry", "5",
+      "--warc-file", ItemInterpolation("%(item_dir)s/%(warc_file_base)s"),
+      "--warc-header", "operator: Archive Team",
+      "--warc-header", "xanga-dld-script-version: " + VERSION,
+      "--warc-header", ItemInterpolation("xanga-usre: %(item_name)s"),
+      ItemInterpolation("http://%(item_name)s.xanga.com/")
+    ],
+    max_tries = 2,
+    accept_on_exit_code = [ 0, 4, 6, 8 ],
+  ),
+  PrepareStatsForTracker(
+    defaults = { "downloader": downloader, "version": VERSION },
+    file_groups = {
+      "data": [ ItemInterpolation("%(item_dir)s/%(warc_file_base)s.warc.gz") ]
+    }
+  ),
+  MoveFiles(),
+  LimitConcurrent(NumberConfigValue(min=1, max=4, default="1", name="shared:rsync_threads", title="Rsync threads", description="The maximum number of concurrent uploads."),
+    UploadWithTracker(
+      "http://tracker.archiveteam.org/%s" % TRACKER_ID,
+      downloader = downloader,
+      version = VERSION,
+      files = [
+        ItemInterpolation("%(data_dir)s/%(warc_file_base)s.warc.gz")
+      ],
+      rsync_target_source_path = ItemInterpolation("%(data_dir)s/"),
+      rsync_extra_args = [
+        "--recursive",
+        "--partial",
+        "--partial-dir", ".rsync-tmp"
+      ]
+    ),
+  ),
+  SendDoneToTracker(
+    tracker_url = "http://tracker.archiveteam.org/%s" % TRACKER_ID,
+    stats = ItemValue("stats")
+  )
+)
+
